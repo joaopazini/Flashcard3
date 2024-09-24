9c443ef521ab642229d3f950df8b238d7422b7e1
joaopazini <160858045+joaopazini@users.noreply.github.com>
2024-09-24 08:04:04 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+function criaCartao (categoria, pergunta, resposta) {
+    console.log(categoria, pergunta, resposta)
+}
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+}
+let respostaEstaVisivel = false
+function viraCartao(){
+    respostaestaVisivel = !respostaEstaVisivel
+}
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
